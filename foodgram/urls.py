877e0728715c038981a008bb56f8ef55dd00e77b
abi83 <vladimir.kromm@yandex.ru>
2021-05-07 15:15:00 +0700

    login page is done

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include("django.contrib.auth.urls")),
+    # path('auth/', include("django.contrib.auth.urls")),
     path('api/v1/', include('apps.api.urls', namespace='api')),
     path('', include('apps.recipes.urls', namespace='recipes')),
+    path('user/', include('apps.users.urls', namespace='users')),
 ]
 
 # TODO: django CDN storage
