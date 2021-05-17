9a748de76321fcc524e56dff8b02f642179fff2f
abi83 <vladimir.kromm@yandex.ru>
2021-05-17 16:47:59 +0700

    PDF generation works

--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -1,7 +1,10 @@
+import datetime
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import ImproperlyConfigured
 from django.db.models import Q, Prefetch, Subquery, OuterRef, Count
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import (ListView, DetailView, CreateView,
@@ -9,19 +12,11 @@ from django.views.generic import (ListView, DetailView, CreateView,
 from django.views.generic.base import View
 from django.views.generic.edit import ModelFormMixin
 
-
-#PDF response
-import io
-from django.http import FileResponse
-from reportlab.pdfgen import canvas
-from django.template import Template, Context
-
-
-
 from apps.recipes.forms import RecipeForm
 from apps.recipes.models import (
     Recipe, RecipeIngredient, Ingredient, Follow, CartItem)
 from apps.recipes.paginator import FixedPaginator
+from apps.recipes.utils import render_to_pdf
 
 User = get_user_model()
 
@@ -241,68 +236,24 @@ def cart_count(request):
         'cart_count': CartItem.objects.filter(user=request.user).count()
     }
 
-# from django_weasyprint import WeasyTemplateResponseMixin
-# from django_weasyprint.views import CONTENT_TYPE_PNG, WeasyTemplateResponse
-
-import os
-from django.http import HttpResponse
-import datetime
-from apps.recipes.utils import render_to_pdf
 
 class ShopList(View):
-    # pass
-#     def get(self, request):
-    #     from django.conf import settings
-    #     import os
-        # # Create a file-like buffer to receive PDF data.
-        # buffer = io.BytesIO()
-        #
-        # # Create the PDF object, using the buffer as its "file."
-        # p = canvas.Canvas(buffer)
-        # # Draw things on the PDF. Here's where the PDF generation happens.
-        # # See the ReportLab documentation for the full list of functionality.
-        # # p.translate(inch, inch)
-        # p.setStrokeColorRGB(0.2, 0.5, 0.3)
-        # p.setFillColorRGB(1, 0, 1)
-        # # draw a rectangle
-        # p.rect(10, 10, 550, 820, fill=1)
-        # # make text go straight up
-        # # p.rotate(90)
-        # # change color
-        # p.setFillColorRGB(0, 0, 0.77)
-        # # say hello (note after rotate the y coord needs to be negative!)
-        # p.drawString(3 * 10, -3 * 10, "Hello World!")
-        # p.drawString(10, 10, "Hello world1.")
-        # p.drawString(10, 20, "Hello world2.")
-        #
-        #
-        # # Close the PDF object cleanly, and we're done.
-        # p.showPage()
-        # p.save()
-        #
-        # # FileResponse sets the Content-Disposition header so that browsers
-        # # present the option to save the file.
-        # buffer.seek(0)
-        # from rlextra.rml2pdf.rml2pdf import Controller
-
-        # t = Template(open(os.path.join(settings.BASE_DIR, 'apps/recipes/templates/recipes/shop-list.rml')).read())
-        # c = Context({"name": 'Wladimir'})
-        # rml = t.render(c)
-        # buffer = io.BytesIO()
-        #
-        # return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
-
-    # model = Recipe
-    # template_name = 'recipes/mymodel.html'
-    # response_class = WeasyTemplateResponse
-    # pdf_filename = 'foo.pdf'
-
     def get(self, request, *args, **kwargs):
+        carts = CartItem.objects.filter(user=self.request.user).select_related('recipe')
+        recipes = Recipe.objects.filter(carts__in=carts)
+        items = RecipeIngredient.objects.filter(recipe__in=recipes)
         data = {
-            'today': datetime.date.today(),
-            'amount': 39.99,
-            'customer_name': 'Wladimir Крёмм',
-            'order_id': 1233434,
+            'time': datetime.datetime.now(),
+            'number': recipes.count(),
+            'customer_name': request.user.get_full_name(),
+            'ingredients': {},
         }
+        for item in items:
+            try:
+                data['ingredients'][item.ingredient.name]['value'] += item.count
+            except KeyError:
+                data['ingredients'][item.ingredient.name] = {'value': item.count, 'unit': item.ingredient.unit}
         pdf = render_to_pdf('recipes/shop-list.html', data)
-        return HttpResponse(pdf, content_type='application/pdf')
+        filename = f'Shop_list_on_{datetime.date.today()}.pdf'
+        return HttpResponse(pdf, content_type='application/pdf',
+                            headers={'Content-Disposition': f"attachment; filename={filename}"})
