ac4c00c627ba7cf90898874865d6ef3f89cd5371
abi83 <vladimir.kromm@yandex.ru>
2021-05-25 17:51:04 +0700

    test_settings file created

--- foodgram/tests_settings.py
+++ foodgram/tests_settings.py
@@ -3,3 +3,10 @@ from foodgram.settings import *  # noqa
 
 LOGGING = None
 SECRET_KEY = 'daf45edee8821f921abc11cbb69058e12d60e9ba69851881fb'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
